0c62314fd13700200855ceace2700f3fa5dac931
diru1100 <rohitdandamudi.1100@gmail.com>
2020-09-26 13:20:49 +0530

    Fix bugs
    - Add reponse code when URL is successfully submitted
    - Anchor href fix

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,7 @@ from redis import Redis
 import flask 
 from .tasks import count_words_at_url
 from app.models import Url
-
+import json
 @app.route("/url", methods=["POST","GET"])
 @app.route("/", methods=["POST","GET"])
 def url():
@@ -14,7 +14,13 @@ def url():
     if flask.request.method == "POST":
         submited_url = flask.request.form["url"]              
         url_count = q.enqueue(count_words_at_url, result_ttl = 10, ttl = 10, args=(submited_url,))
-               
+        response = 200
+        if(url_count is not None or url_count["error"] is not None):
+            reponse = 404
+
+
+        return render_template("submit_url.html"), response, {'ContentType':'application/json', 'extra':url_count}
+
     return render_template("submit_url.html")
 
 
@@ -22,5 +28,5 @@ def url():
 def index():
     url_details = Url.query.all()
 
-    return render_template("index.html", url_details = url_details)
+    return render_template("index.html", url_details = url_details), 200, {'data':url_details}
 
